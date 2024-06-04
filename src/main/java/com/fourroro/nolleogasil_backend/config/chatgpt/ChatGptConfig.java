b3ea4a7d854f213de5d592c76d5e343e234e5b08
Chony <chdms7471@naver.com>
2024-06-04 20:11:40 +0900

    add classpath: @Value ChatGptConfig.java

--- src/main/java/com/fourroro/nolleogasil_backend/config/chatgpt/ChatGptConfig.java
+++ src/main/java/com/fourroro/nolleogasil_backend/config/chatgpt/ChatGptConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ChatGptConfig {
-    @Value("${openai.api.key}")
+    @Value("${classpath:openai.api.key}")
     private String openAiKey;
     @Bean
     public RestTemplate template(){
