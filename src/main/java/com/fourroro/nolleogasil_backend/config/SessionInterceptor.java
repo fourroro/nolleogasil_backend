a101029f4b521473ceeef4a8bad28ce55d285919
Chony <chdms7471@naver.com>
2024-06-20 22:30:02 +0900

    add print logging in SessionInterceptor

--- src/main/java/com/fourroro/nolleogasil_backend/config/SessionInterceptor.java
+++ src/main/java/com/fourroro/nolleogasil_backend/config/SessionInterceptor.java
@@ -3,12 +3,16 @@ package com.fourroro.nolleogasil_backend.config;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
 public class SessionInterceptor implements HandlerInterceptor {
+    private static final Logger logger = LoggerFactory.getLogger(SessionInterceptor.class);
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
@@ -18,10 +22,12 @@ public class SessionInterceptor implements HandlerInterceptor {
         //세션 만료 여부 확인
         if(!isSessionExpired(request)){
             response.setStatus(HttpStatus.OK.value());
+            logger.info("Session is valid.");
             return true;
         }else {
             //세션이 만료되었다면, 401 상태 코드 전달
             response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            logger.info("Session is invalid or expired.");
             return false;
         }
     }
