f091e7ddac590930d4c999f9d207df8c661264e8
Chony <chdms7471@naver.com>
2024-06-26 12:51:14 +0900

    remove annotation in RedisConfig

--- src/main/java/com/fourroro/nolleogasil_backend/config/RedisConfig.java
+++ src/main/java/com/fourroro/nolleogasil_backend/config/RedisConfig.java
@@ -18,7 +18,6 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
-@EnableRedisRepositories
 public class RedisConfig {
 
     @Value("${spring.data.redis.host}")
