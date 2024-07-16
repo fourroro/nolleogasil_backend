d15f9fcd0a50b9c97a00d3e032badd20a2291eb4
Chony <chdms7471@naver.com>
2024-07-16 15:51:04 +0900

    redisConfig modified with simple version

--- src/main/java/com/fourroro/nolleogasil_backend/NolleogasilBackendApplication.java
+++ src/main/java/com/fourroro/nolleogasil_backend/NolleogasilBackendApplication.java
@@ -8,6 +8,7 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableFeignClients
+@EnableRedisHttpSession
 public class NolleogasilBackendApplication {
 
     public static void main(String[] args) {
