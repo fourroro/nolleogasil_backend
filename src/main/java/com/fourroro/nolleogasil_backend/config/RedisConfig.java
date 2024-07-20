22261445d937368e867c54bd5522ea9b666b2b75
hyouzl <hyouzl0521@gmail.com>
2024-07-20 23:29:26 +0900

    redis config cookie domain 수정

--- src/main/java/com/fourroro/nolleogasil_backend/config/RedisConfig.java
+++ src/main/java/com/fourroro/nolleogasil_backend/config/RedisConfig.java
@@ -61,7 +61,7 @@ public class RedisConfig {
         DefaultCookieSerializer serializer = new DefaultCookieSerializer();
         serializer.setCookieName("JSESSIONID");
         serializer.setCookiePath("/");
-        serializer.setDomainName("backend.nolleogasil.com");  // 도메인 설정
+        serializer.setDomainName(".nolleogasil.com");  // 도메인 설정
         serializer.setUseHttpOnlyCookie(true);
         serializer.setSameSite("None");
         serializer.setUseSecureCookie(true);
