28d21c55a9376e2ccb06d9923b62cee46dd16e4d
Chony <chdms7471@naver.com>
2024-07-20 21:38:19 +0900

    change session.redis.namespace spring:session -> nolleogasil:session

--- src/main/java/com/fourroro/nolleogasil_backend/config/RedisConfig.java
+++ src/main/java/com/fourroro/nolleogasil_backend/config/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.fourroro.nolleogasil_backend.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
