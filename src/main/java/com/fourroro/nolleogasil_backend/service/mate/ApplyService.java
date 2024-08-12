0ab449feb00a8f362fe8c9c16232645c4b69cc36
Chony <chdms7471@naver.com>
2024-08-12 12:21:29 +0900

    add javadoc to service

--- src/main/java/com/fourroro/nolleogasil_backend/service/mate/ApplyService.java
+++ src/main/java/com/fourroro/nolleogasil_backend/service/mate/ApplyService.java
@@ -1,5 +1,5 @@
 /**
- * 맛집메이트의 신청 관리를 위한 Service Interface입니다.
+ * 맛집메이트의 신청 관리를 위한 Service interface입니다.
  * @author 박초은
  * @since 2024-01-05
  */
@@ -11,28 +11,28 @@ import java.util.List;
 
 public interface ApplyService {
 
-    //insert apply
+    /** insert apply */
     public void insertApply(ApplyDto applyDto);
 
-    //해당 apply의 isApply값 변경
+    /** 해당 apply의 isApply값 변경 */
     public void updateIsApply(ApplyDto applyDto);
 
-    //보낸 신청 목록 조회
+    /** 보낸 신청 목록 조회 */
     public List<ApplyDto> getSendApplyList(Long applicantId);
 
-    //받은 신청 목록 조회
+    /** 받은 신청 목록 조회 */
     public List<ApplyDto> getReceiveApplyList(Long usersId);
 
-    //1개의 apply 조회(applyId 이용)
+    /** 1개의 apply 조회(applyId 이용) */
     public ApplyDto getApply(Long applyId);
 
-    //1개의 apply 조회(mateId, applicantId(usersId) 이용)
+    /** 1개의 apply 조회(mateId, applicantId(usersId) 이용) */
     public ApplyDto getApplyByMateIdAndUsersId(Long mateId, Long usersId);
 
-    //apply 유무 확인
+    /** apply 유무 확인 */
     public boolean checkApplyColumn(Long mateId, Long usersId);
 
-    //apply 삭제
+    /** apply 삭제 */
     public void deleteApply(Long applyId);
 
 }
